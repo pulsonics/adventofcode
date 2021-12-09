6e9b02808358492e59f1b90355eabc74b95228cd
Joseph Longhi <longhi@hawaii.edu>
2021-12-08 23:15:24 -0800

    Day 1 Part 2 Solution

--- 2021/d1/d1.cpp
+++ 2021/d1/d1.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <string>
 #include <optional>
+#include <deque>
+#include <algorithm>
 
-int main() 
+static int part_one() 
 {
     std::string line;
     std::optional<int> last_value;
-    size_t larger_count = 0;
+    size_t increasing = 0;
 
     while(std::getline(std::cin, line)) {
         auto val = stoi(line);
@@ -16,12 +18,48 @@ int main()
         }
 
         if (val > last_value) {
-            larger_count++;
+            increasing++;
         }
 
         last_value = val;
     }
 
-    std::cout << "`" << larger_count << "`" << std::endl;
+    return increasing;
+}
+
+static int part_two()
+{
+    std::string line;
+    std::deque<int> window;
+    int total = 0;
+    int last_total;
+    int increasing = 0;
+
+    while(std::getline(std::cin, line)) {
+        auto val = stoi(line);
+
+        if (window.size() != 3 ) {
+            window.push_front(val);
+            total += val;
+            continue;
+        }
+
+        last_total = total;
+        total += val;
+        total -= window.front();
+        window.pop_front();
+        window.push_back(val);
+
+        if (total > last_total) {
+            increasing++;
+        }
+    }
+
+    return increasing;
+}
+
+int main() 
+{
+    std::cout << "`" << part_two() << "`" << std::endl;
     return 0;
 }
